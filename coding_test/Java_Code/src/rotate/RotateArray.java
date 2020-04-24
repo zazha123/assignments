c4d18a43f8e9c60323c820b5786c6b43203b8266
zhazha <42703555+zazha123@users.noreply.github.com>
2020-04-24 18:09:25 -0400

    add some tests

--- coding_test/Java_Code/src/rotate/RotateArray.java
+++ coding_test/Java_Code/src/rotate/RotateArray.java
@@ -2,6 +2,7 @@ package rotate;
 
 public class RotateArray {
     public static int[] solve(int a[], int n, int k) {
+        if(a == null || a.length == 0) return a;
         //reverse all
         reverse(a, 0, n - 1);
         //reverse first k
@@ -12,6 +13,7 @@ public class RotateArray {
     }
 
     public static void reverse(int a[], int l, int r) {
+        if(l >= r) return;
         for(int i = 0; i <= (r - l) / 2; i++) {
             swap(a, l + i, r - i);
         }
--- coding_test/Java_Code/src/rotate/Test.java
+++ coding_test/Java_Code/src/rotate/Test.java
@@ -6,9 +6,22 @@ public class Test {
     public static void main(String [] args) throws Exception {
         System.out.println("---test---");
         int[] input = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println("input: " + Arrays.toString(input));
+        System.out.println("input: " + Arrays.toString(input) + " k = 3");
         int[] output = RotateArray.solve(input, input.length, 3);
         System.out.println("output: " + Arrays.toString(output));
+
+        System.out.println("---test2---");
+        int[] input2 = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println("input: " + Arrays.toString(input2) + " k = 0");
+        output = RotateArray.solve(input2, input2.length, 0);
+        System.out.println("output: " + Arrays.toString(output));
+
+        System.out.println("---test3---");
+        int[] input3 = {};
+        System.out.println("input: " + Arrays.toString(input3) + " k = 3");
+        output = RotateArray.solve(input3, input3.length, 3);
+        System.out.println("output: " + Arrays.toString(output));
+
     }
 }
 
